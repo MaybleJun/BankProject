fefb94a54b06c11a715b812c57b706d30b5814be
MaybleJul <mayblejul@gmail.com>
2024-06-11 15:29:01 +0400

    add: routes, Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, { Suspense } from 'react';
+import {Route, Routes, Link} from 'react-router-dom'
 import './index.scss';
+import { HomePageAsync } from './pages/HomePage/HomePage.async';
+import { LoanPageAsync } from './pages/LoanPage/LoanPage.async';
 
 const App = () => {
     return (
         <div className="app">
-            First style
-            <Counter />
+            <Link to={'/'}>NeoBank</Link>
+            <Link to={'/loan'}>Credit card</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<HomePageAsync />}/>
+                    <Route path={'/loan'} element={<LoanPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/LoanPage/LoanPage.async.tsx
@@ -0,0 +1,3 @@
+import {lazy} from "react";
+
+export const LoanPageAsync = lazy(() => import('./LoanPage'));
