881ff0e35cfce0f0805c9dc73d9e1bacd5d0e639
MaybleJul <mayblejul@gmail.com>
2024-07-15 17:12:34 +0400

    add: Divider component

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Divider.scss';
+
+type DividerProps = {
+  styles?: React.CSSProperties;
+};
+
+export function Divider({ styles }: DividerProps) {
+  return (
+    <span className="Divider">
+      <span className="Divider__line" style={styles}></span>
+    </span>
+  );
+}
