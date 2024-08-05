212345ec6c924575a76f3496728dfddb8d6715f8
MaybleJul <mayblejul@gmail.com>
2024-08-05 19:35:14 +0400

    fix: tableDocument

--- src/components/TableDocument/TableDocument.tsx
+++ src/components/TableDocument/TableDocument.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useSelectorTyped } from "../../hooks/useTypeReduxStore";
-import { generateDocuments,fetchPaymentList, resetLoanState } from "../../store/slice/loanSlice";
+import { generateDocuments, fetchPaymentList, resetLoanState } from "../../store/slice/loanSlice";
 import { AppState } from "../../store/store";
 import { Button } from '../../components/Button/Button';
 import { Loader } from "../Loader/Loader";
@@ -12,7 +12,7 @@ import { Checkbox } from "../../ui/Checkbox/Checkbox";
 
 import "./TableDocument.scss";
 
-const columns = ["NUMBER", "DATE", "TOTAL PAYMENT", "INTEREST PAYMENT", "DEBT PAYMENT", "REMAINING DEBT"];
+const initialColumns = ["NUMBER", "DATE", "TOTAL PAYMENT", "INTEREST PAYMENT", "DEBT PAYMENT", "REMAINING DEBT"];
 
 const TableDocument = () => {
     const dispatch = useAppDispatch();
@@ -23,6 +23,7 @@ const TableDocument = () => {
     const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
     const [isPopupOpened, setIsPopupOpened] = useState<boolean>(false);
     const [isUserAgree, setIsUserAgree] = useState<boolean>(false);
+    const [columns, setColumns] = useState<string[]>(initialColumns);
 
     useEffect(() => {
         if (paymentList === null) dispatch(fetchPaymentList(applicationId as string));
@@ -38,30 +39,29 @@ const TableDocument = () => {
                 </div>
                 {paymentList ? <Table columns={columns} data={paymentList} /> : <Loader />}
                 <div className="table-wrapper__bottom">
-                        <Button className="Button table-wrapper__bottom__BtnDeny" onClick={() => setIsModalOpened(true)}>
-                            Deny
-                         </Button>
+                    <Button className="Button table-wrapper__bottom__BtnDeny" onClick={() => setIsModalOpened(true)}>
+                        Deny
+                    </Button>
                     <div className="table-wrapper__bottom__checks">
-                    <Checkbox setIsChecked={setIsUserAgree} labelText="I agree with the payment schedule" isChecked={isUserAgree} />
+                        <Checkbox setIsChecked={setIsUserAgree} labelText="I agree with the payment schedule" isChecked={isUserAgree} />
                         <Button
                             type='submit'
                             className="Button table-wrapper__bottom__BtnSend"
-                             disabled={!isUserAgree}
+                            disabled={!isUserAgree}
                             onClick={() => {
-                            if (applicationId) {
-                                dispatch(generateDocuments(applicationId));
-                            }
-                             }}
+                                if (applicationId) {
+                                    dispatch(generateDocuments(applicationId));
+                                }
+                            }}
                         >
                             Send
                         </Button>
-
                     </div>
                 </div>
             </div>
             <Modal
                 title="Deny application"
-                message="You exactly sure, you want to cancel this application?"
+                message="Are you sure you want to cancel this application?"
                 onClose={() => setIsModalOpened(false)}
                 onClick={() => {
                     setIsModalOpened(false);
@@ -71,7 +71,7 @@ const TableDocument = () => {
             />
             <Popup
                 title="Deny application"
-                message="Your application has been deny!"
+                message="Your application has been denied!"
                 isOpened={isPopupOpened}
                 onClose={() => {
                     dispatch(resetLoanState());
